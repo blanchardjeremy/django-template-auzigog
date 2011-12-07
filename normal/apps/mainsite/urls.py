8b10fa128466ee526c5e275842c500453130ef1f
Jeremy Blanchard <auzigog@gmail.com>
2011-12-06 23:31:21 -0800

    Add SAMPLEAPP to make getting started process quicker

--- normal/apps/mainsite/urls.py
+++ normal/apps/mainsite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
+from mainsite.views import *
 
 
 admin.autodiscover()
@@ -11,7 +12,9 @@ handler404 = 'mainsite.views.error404'
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^test/$', TestView.as_view(), name='test'),
 
+    (r'', include('SAMPLEAPP.urls')),
 )
 
 
